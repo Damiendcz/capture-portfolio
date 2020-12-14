561a82a64b1620135ac77fb62aa9e0fe13a77878
DamienDcz <damiendescroizette@gmail.com>
2020-12-14 12:53:44 +0100

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,8 @@
+import React from "react";
+//Page component
+import AboutSection from "../components/AboutSection";
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutSection;
